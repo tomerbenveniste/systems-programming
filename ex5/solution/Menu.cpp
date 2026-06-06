ed90d4b8296f54638f45ffeba1ebdaf2eb176d92
carmif <carmifrank@gmail.com>
2026-06-07 02:21:34 +0300

    Added comments

--- ex5/solution/Menu.cpp
+++ ex5/solution/Menu.cpp
@@ -1,41 +1,49 @@
+// Menu.cpp
+// iostream is a library in C++ that provides input and output functionality. It allows us to read from the standard input (keyboard) and write to the standard output (console).
 #include <iostream>
+// We include the header file "Menu.h" which contains the declaration of the Menu class. This allows us to use the Menu class and its member functions in this implementation file.
 #include "Menu.h"
-#include "MyQueue.h"
-
+//the using namespace std; directive allows us to use names from the standard library (like cout and cin) without having to prefix them with std::.
 using namespace std;
 
+// The mainMenu function is the entry point of the menu system. It displays the main menu options and handles user input to navigate to the stack menu, queue menu, or exit the program.
 void Menu::mainMenu() {
+    // We use an infinite loop to continuously display the main menu until the user chooses to exit.
     while (true) {
+        // Display the main menu options to the user.
         cout << "Main Menu" << endl;
         cout << "1. stackMenu" << endl;
         cout << "2. queueMenu" << endl;
         cout << "3. Exit" << endl;
+        // Read the user's input for menu selection.
         int user_input;
         cin >> user_input;
+        // Based on the user's input, we call the appropriate menu function or exit the program.
         if (user_input == 1) {
         cout << "stackMenu" << endl;
+        // If the user selects option 1, we call the stackMenu function to display the stack menu and handle stack operations.
         this->stackMenu();
         }
         else if (user_input == 2) {
+            // If the user selects option 2, we call the queueMenu function to display the queue menu and handle queue operations.
             cout << "queueMenu" << endl;
             this->queueMenu();
         }
         else if (user_input == 3) {
+            // If the user selects option 3, we print "Exit" and terminate the program using exit(0).
             cout << "Exit" << endl;
             exit(0);
             
         }
         else {
+            // If the user enters an invalid option, we display an error message and prompt them to try again.
             cout << "Invalid input. Please try again." << endl;
         }
     }
   
 
 }
-void Menu::input_manager(int user_input) {
-    
-
-}
+// The stackMenu function displays the stack menu options and handles user input to perform stack operations such as push, pop, check if the stack is empty, print the stack, or return to the main menu.
 void Menu::stackMenu() {
     int user_input;
     while (true) {
@@ -47,6 +55,8 @@ void Menu::stackMenu() {
         cout << "5. Return to Main Menu" << endl;
         int user_input;
         cin >> user_input;
+        // Based on the user's input, we perform the corresponding stack operation.
+        // If the user selects option 1, we prompt them to enter an element to push onto the stack, read the input, and call the push function of the stack.
         if (user_input == 1) {
                 cout << "Enter element to push: ";
                 int* element = new int;
@@ -54,19 +64,23 @@ void Menu::stackMenu() {
                 this->stack.push(*element);
                 delete element;
         }
+        // If the user selects option 2, we call the pop function of the stack to remove the top element.
         else if (user_input == 2)
         {
             this->stack.pop();
         }
+        // If the user selects option 3, we check if the stack is empty and print the appropriate message.
         else if (user_input == 3)
         {
             cout << "Stack is " << (this->stack.isEmpty() ? "empty" : "not empty") << endl;
         }
+        // If the user selects option 4, we call the print_stack function of the stack to display the contents of the stack.
         else if (user_input == 4)
         {
             cout << "Print Stack" << endl;
             this->stack.print_stack();  
         }
+        // If the user selects option 5, we break out of the loop and return to the main menu.
         else if (user_input == 5)
         {
             break;
@@ -77,6 +91,7 @@ void Menu::stackMenu() {
     }   
 
 }
+// The queueMenu function displays the queue menu options and handles user input to perform queue operations such as printing the queue, adding an element, removing an element, printing the first element, or returning to the main menu.
 void Menu::queueMenu() {
     while (true) {
         if(this->queue.get_maxQ() == 0) {
@@ -93,10 +108,13 @@ void Menu::queueMenu() {
         cout << "5. Return to Main Menu" << endl;
         int user_input;
         cin >> user_input;
+        // Based on the user's input, we perform the corresponding queue operation.
+        // If the user selects option 1, we call the print_queue function of the queue
         if (user_input == 1) {
             cout << "Print Queue" << endl;  
             this->queue.print_queue();
         }   
+        // If the user selects option 2, we check if the queue is full. If it is full, we display a message and return to the queue menu. Otherwise, we prompt the user to enter an element to add to the queue, read the input, and call the enQueue function of the queue.
         else if (user_input == 2)
         {
             if (this->queue.is_full()) {
@@ -110,16 +128,19 @@ void Menu::queueMenu() {
             bool result = this->queue.enQueue(*element);
             delete element;
         }
+        // If the user selects option 3, we call the deQueue function of the queue to remove the front element.
         else if (user_input == 3)
         {
             cout << "Remove Element" << endl;
             this->queue.deQueue();
         }
+        // If the user selects option 4, we call the peek function of the queue to get the front element and print it.
         else if (user_input == 4)
         {
             cout << "Print First Element" << endl;
             cout << this->queue.peek() << endl;
         }
+        // If the user selects option 5, we break out of the loop and return to the main menu.
         else if (user_input == 5)
         {
             break;
@@ -129,20 +150,25 @@ void Menu::queueMenu() {
         }
     }
 }
+
+// The constructor of the Menu class initializes
 Menu::Menu() : queue(0), stack() {
-    cout << "Menu constructor called" << endl;
+    // No additional initialization needed
 }
+// The destructor of the Menu class is called when an object of the Menu class goes out of scope or is explicitly deleted. It prints a message indicating that the destructor has been called.
 Menu::~Menu() {
-    cout << "Menu destructor called" << endl;
-}
+    // No manual cleanup needed since we are using standard library containers and dynamic memory is managed within those containers.
+    }
 
 
-
+// The main function is the entry point of the program. It creates an instance of the Menu class and calls the mainMenu function to start the menu system. Finally, it returns 0 to indicate that the program has ended successfully.
 int main()
 {
-
-     Menu menu;
+    // We create an instance of the Menu class called menu.
+    Menu menu;
+    // We call the mainMenu function of the menu object to start the menu system and allow the user to interact with it.
     menu.mainMenu();
+    // We return 0 to indicate that the program has ended successfully.
     return 0;
     
 }
--- ex5/solution/MyQueue.cpp
+++ ex5/solution/MyQueue.cpp
@@ -2,25 +2,29 @@
 #include "MyQueue.h"
 
 using namespace std;
+// The default constructor of the MyQueue class initializes
 MyQueue::MyQueue() {
     this->maxQ = 0;
 }
+// The parameterized constructor of the MyQueue class initializes
 MyQueue::MyQueue(int maxQ) {
     this->maxQ = maxQ;
 }
-
+// The destructor of the MyQueue class 
 MyQueue::~MyQueue() {
     // No manual delete needed
 }
-
+// The set_maxQ function sets the maximum capacity of the queue to the given value.
 void MyQueue:: set_maxQ(int maxQ) {
     this->maxQ = maxQ;
 }
 
+// The get_maxQ function returns the maximum capacity of the queue.
 int MyQueue:: get_maxQ() const {
     return this->maxQ;
 }
 
+// The print_queue function prints the elements of the queue. If the queue is empty, it prints a message indicating that the queue is empty.
 void MyQueue::print_queue() const {
     if (isEmpty()) {
         cout << "Queue is empty" << endl;
@@ -33,7 +37,7 @@ void MyQueue::print_queue() const {
 
     cout << endl;
 }
-
+// The enQueue function adds an element to the back of the queue. If the queue is full (i.e., its size is greater than or equal to maxQ), it prints a message indicating that the queue is full and returns false. Otherwise, it adds the element to the back of the queue and returns true.
 bool MyQueue::enQueue(int element) {
     if (queue.size() >= maxQ) {
         cout << "Queue is full" << endl;
@@ -43,7 +47,7 @@ bool MyQueue::enQueue(int element) {
     return true;
 }
 
-
+// The deQueue function removes an element from the front of the queue. If the queue is empty, it prints a message indicating that the queue is empty and returns false. Otherwise, it removes the front element from the queue and returns true.
 bool MyQueue::deQueue() {
     if (isEmpty()) {
         cout << "Queue is empty" << endl;
@@ -54,6 +58,7 @@ bool MyQueue::deQueue() {
     return true;
 }
 
+// The peek function returns the front element of the queue without removing it. If the queue is empty, it returns -1 to indicate that the queue is empty. Otherwise, it returns the front element of the queue.
 int MyQueue::peek() const {
     if (isEmpty()) {
         return -1; // Return -1 to indicate the queue is empty
@@ -62,10 +67,12 @@ int MyQueue::peek() const {
     return queue[0];
 }
 
+// The isEmpty function checks if the queue is empty. It returns true if the queue is empty and false otherwise.
 bool MyQueue::isEmpty() const {
     return queue.empty();
 }
 
+// The is_full function checks if the queue is full. It returns true if the size of the queue is greater than or equal to maxQ and false otherwise.
 bool MyQueue::is_full() const {
     return queue.size() >= maxQ;
 }
